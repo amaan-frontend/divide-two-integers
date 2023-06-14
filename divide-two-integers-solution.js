f945079657d6ae42a579fb44b3c033f2b36dc6fc
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-06-14 23:48:01 +0530

    Solution upload
    Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
    The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.

---
+++ divide-two-integers-solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function (dividend, divisor) {
+    if (divisor === 0) {
+        return Math.sign(dividend) * Infinity;
+    }
+      dividend -= divisor;
+        quotient++;
+    }
+
+    return isNegative ? -quotient : quotient;
+
